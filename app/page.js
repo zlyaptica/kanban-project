e60bf2ae3c0eb1c324530c10e1a21a746394c151
zlyaptica <maximov021@mail.ru>
2024-03-19 15:10:54 +0300

    add navbar v1

--- app/page.js
+++ app/page.js
@@ -3,12 +3,19 @@ import avatar from '../public/home.jpg';
 import Link from 'next/link';
 
 
-export default function Home() {
+export default async function Home() {
+    
+    let data = await getdata()
 
     return (
         <>
+            <h1>{data}</h1>
             <Link href={'/kanban'}>kanban</Link>
             <div className='content'><Image alt="home" src={avatar} placeholder="blur" /></div>
         </>
-    );
+    )
+}
+
+async function getdata() {
+    return "testData"
 }
