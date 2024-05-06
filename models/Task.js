0e170a461349c9fe306d41d6251baee75cf251c1
Denis Piyalkin <pialkind@mail.ru>
2024-05-06 14:40:36 +0300

    updated models/Task

--- models/Task.js
+++ models/Task.js
@@ -7,7 +7,12 @@ const taskSchema = new Schema({
     status: { type: Schema.Types.ObjectId, ref: 'Status' },
     name: String,
     description: String,
-    index: Number
+    index: Number,
+    done: Boolean,
+    creationDate: Date,
+    startDate: Date,
+    deadLineDate: Date,
+    priority: Number
 });
 
 
