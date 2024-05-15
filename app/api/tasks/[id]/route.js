2a3e41e12d5d674beb3888b2c5375db34143a3f7
Denis Piyalkin <pialkind@mail.ru>
2024-05-15 13:36:30 +0300

    tasks/[id] changes

--- app/api/tasks/[id]/route.js
+++ app/api/tasks/[id]/route.js
@@ -1,10 +1,11 @@
-import { GetBoardData, GetStatusData } from "@/utils/utility_methods";
+import { GetBoardData, GetStatusData, GetTaskStruct } from "@/utils/utility_methods";
 import dbConnect from "@/lib/dbConnect";
 import Task from "@/models/Task";
 import { NextResponse } from "next/server";
 import Subtask from "@/models/Subtask";
 import Status from "@/models/Status";
 import Board from "@/models/Board";
+import User from "@/models/User";
 
 export async function DELETE(request, { params }) {
   await dbConnect();
@@ -37,6 +38,9 @@ export async function DELETE(request, { params }) {
 }
 
 export async function POST(request, { params }) {
+  let resultStatus = "success";
+  let boardData;
+  let updatedTask;
   await dbConnect();
 
   const task_id = params.id;
@@ -45,26 +49,123 @@ export async function POST(request, { params }) {
 
   let task = await Task.findOne({ _id: task_id });
   const board = await Board.findOne({ _id: task.board_id });
-
   if (data.author_id == board.author) {
-    if (data.type == "name") {
+    if (data.field == "name") {
       await Task.findByIdAndUpdate(task_id, { name: data.name });
     }
-    let boardData = await GetBoardData(task.board_id);
-    let updatedTask = await Task.findOne({ _id: task_id });
+    if (data.field == "description") {
+      await Task.findByIdAndUpdate(task_id, {
+        description: data.description,
+      });
+    }
+    if (data.field == "is_completed") {
+      await Task.findByIdAndUpdate(task_id, {
+        is_completed: data.is_completed,
+      });
+    }
+    if (data.field == "priority") {
+      await Task.findByIdAndUpdate(task_id, {
+        priority: data.priority,
+      });
+    }
+
+    if (data.field == "doer") {
+      const foundUser = await User.findOne({ email: data.doerEmail });
+      if (foundUser) {
+        if (data.type == "set") {
+          await Task.findByIdAndUpdate(task_id, {
+            doer: foundUser._id,
+          });
+        }
+        if (data.type == "unset") {
+          await Task.findByIdAndUpdate(task_id, {
+            $unset: { doer: foundUser._id },
+          });
+        }
+      } else {
+        resultStatus = "user not found";
+      }
+    }
+
+    if (data.field == "deadLineDate") {
+      if (data.deadLineDate) {
+        if (task.startDate && new Date(data.deadLineDate) < task.startDate) {
+          resultStatus = "wrong range";
+        } else {
+          resultStatus = "current range for deadline";
+        }
+      } else {
+        await Task.findByIdAndUpdate(task_id, {
+          $unset: { deadLineDate: data.deadLineDate },
+        });
+      }
+    }
+    if (data.field == "startDate") {
+      if (data.startDate) {
+        if (task.deadLineDate && new Date(data.startDate) > task.deadLineDate) {
+          resultStatus = "wrong range";
+        } else {
+          resultStatus = "current range for startDate";
+        }
+      } else {
+        await Task.findByIdAndUpdate(task_id, {
+          $unset: { startDate: data.startDate },
+        });
+      }
+    }
+  } else {
+    resultStatus = "error";
+  }
+
+  if (resultStatus == "user not found") {
     return NextResponse.json(
       {
-        message: "task updated",
-        boardData: boardData,
-        updatedTask: updatedTask,
+        message: "неверно указана почта пользователя",
       },
-      { status: 200 }
+      { status: 400 }
     );
   }
+
+  if (resultStatus == "current range for deadline") {
+    await Task.findByIdAndUpdate(task_id, {
+      deadLineDate: data.deadLineDate,
+    });
+  }
+
+  if (resultStatus == "current range for startDate") {
+    await Task.findByIdAndUpdate(task_id, {
+      startDate: data.startDate,
+    });
+  }
+
+  if (resultStatus == "wrong range") {
+    return NextResponse.json(
+      {
+        message: "неверно указан диапазон дат",
+      },
+      { status: 400 }
+    );
+  }
+
+  if (resultStatus == "error") {
+    return NextResponse.json(
+      {
+        message: "вы не создатель доски",
+      },
+      { status: 403 }
+    );
+  }
+
+  boardData = await GetBoardData(task.board_id);
+  let taskData = await Task.findOne({ _id: task_id })
+  updatedTask = await GetTaskStruct(taskData)
+
   return NextResponse.json(
     {
-      message: "вы не создатель доски",
+      message: "поле успешно изменено",
+      boardData: boardData,
+      updatedTask: updatedTask,
     },
-    { status: 403 }
+    { status: 200 }
   );
 }
